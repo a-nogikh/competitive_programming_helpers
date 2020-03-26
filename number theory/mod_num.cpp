478e0fa3a229229860338b804e8594d8c8279a7e
Aleksandr Nogikh <a.nogikh@yandex.ru>
2020-03-26 19:19:17 +0300

    Enhanced ModNum; Added functions for factorial and binomial coefficient evaluation

--- number theory/mod_num.cpp
+++ number theory/mod_num.cpp
@@ -2,14 +2,11 @@
 	Class that encapsulates modulo operations
 */
 
-#define MOD 1000000007  // Must be prime!
-
-
+template<int mod>
 class ModNum
 {
 private:
 	int num;
-	const int mod = MOD;
 
 	inline int easy_mod(int value) const
 	{
@@ -19,19 +16,19 @@ private:
 	}
 
 public:
-	ModNum(const int value): num(value) {
+	ModNum(const int value) : num(value) {
 		if (num < 0)
 			num += mod * (-num / mod + 1);
 		else if (num > mod)
 			num = num % mod;
 	}
-	
+
 	operator int() const
 	{
 		return num;
 	}
 
-	ModNum& operator=(const ModNum& rhs)
+	ModNum& operator=(const ModNum<mod>& rhs)
 	{
 		this->num = rhs.num;
 		return *this;
@@ -39,7 +36,7 @@ public:
 
 	ModNum operator +(const ModNum& rhs) const { return ModNum(easy_mod(rhs.num + num)); }
 
-	ModNum& operator +=(const ModNum& rhs) 
+	ModNum& operator +=(const ModNum& rhs)
 	{
 		this->num = easy_mod(num + rhs.num);
 		return *this;
@@ -47,15 +44,21 @@ public:
 
 	ModNum operator -(const ModNum& rhs) const { return ModNum(easy_mod(num + mod - rhs.num)); }
 
+	ModNum& operator -=(const ModNum& rhs)
+	{
+		this->num = easy_mod(num + mod - rhs.num);
+		return *this;
+	}
+
 	ModNum operator *(const int& rhs) const
 	{
-		long long x = (long long) num * rhs;
+		long long x = (long long)num * rhs;
 		return ModNum(x < mod ? x : (x % mod));
 	}
 
 	ModNum& operator *=(const int& rhs)
 	{
-		long long x = (long long) num * rhs;
+		long long x = (long long)num * rhs;
 		this->num = x < mod ? (int)x : (x % mod);
 		return *this;
 	}
@@ -95,3 +98,70 @@ public:
 		return this->pow((const unsigned long long)power);
 	}
 };
+
+template<int mod>
+class FactorialList
+{
+private:
+	vector<ModNum<mod>> precalculated;
+public:
+	FactorialList()
+	{
+		precalculated.resize(1, 1);
+	}
+
+	inline ModNum<mod> get(int n)
+	{
+		while (precalculated.size() <= n)
+			precalculated.push_back(precalculated.back() * (int)precalculated.size());
+		return precalculated[n];
+	}
+
+	ModNum<mod> operator[] (const int n)
+	{
+		return get(n);
+	}
+};
+
+template<int mod>
+class BinomialCalculator
+{
+private:
+	FactorialList<mod> fact;
+
+	class MatrixProxy
+	{
+		int n;
+		BinomialCalculator& outer;
+	public: 
+		MatrixProxy(int n, BinomialCalculator& outer) : n(n), outer(outer) {}
+		ModNum<mod> operator[] (const int k)
+		{
+			return outer.get(n,k);
+		}
+	};
+
+public:
+	ModNum<mod> get(int n, int k)
+	{
+		return fact[n] / (fact[k] * fact[n - k]);
+	}
+
+	MatrixProxy operator[] (const int n)
+	{
+		return MatrixProxy(n, *this);
+	}
+};
+
+template<int mod>
+ModNum<mod> pow(ModNum<mod> base, int power)
+{
+	return base.pow(power);
+}
+
+template<int mod>
+ModNum<mod> bin_coef(int n, int k)
+{
+	static BinomialCalculator<mod> calculator;
+	return calculator.get(n, k);
+}
